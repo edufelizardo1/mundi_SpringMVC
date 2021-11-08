5f0204726aceab8a83666598001772bc8cfa242a
Cândido <edufelizardo1@gamil.com>
2021-11-08 19:01:07 -0300

    Create MundiSpringMvcApplicationTests.java

---
+++ src/test/java/com/edufelizardo/mundi/mundi_springmvc/MundiSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.mundi.mundi_springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundiSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
