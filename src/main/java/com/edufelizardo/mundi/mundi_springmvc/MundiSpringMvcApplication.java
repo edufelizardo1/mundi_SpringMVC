33eca3e89d22752e07224e6f46dab6b64adff209
Cândido <edufelizardo1@gamil.com>
2021-11-08 19:01:29 -0300

    Create MundiSpringMvcApplication.java

---
+++ src/main/java/com/edufelizardo/mundi/mundi_springmvc/MundiSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.mundi.mundi_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundiSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MundiSpringMvcApplication.class, args);
+    }
+
+}
