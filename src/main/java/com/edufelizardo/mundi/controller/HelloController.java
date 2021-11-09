b44afddccb8cf7031c4473cf20b78b04778a04b2
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-11-09 16:57:42 -0300

    Update HelloController.java

--- src/main/java/com/edufelizardo/mundi/controller/HelloController.java
+++ src/main/java/com/edufelizardo/mundi/controller/HelloController.java
@@ -9,10 +9,10 @@ import javax.servlet.http.HttpServletRequest;
  * @author edufelizardo
  * @version 1.0.0
  */
-@Controller
+
 public class HelloController {
 
-    @GetMapping("/hello")
+
     public String hello(HttpServletRequest httpServletRequest) {
         httpServletRequest.setAttribute("nome", "Mundo!");
         return "hello";
